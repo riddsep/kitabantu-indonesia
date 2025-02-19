3935194647ddba9482168cb7cfe4711b36cde337
riddsep <ridoseptiawan00@gmail.com>
2025-02-19 16:24:37 +0700

    fix: import component job

--- src/components/aboutUs.tsx
+++ src/components/aboutUs.tsx
@@ -1,20 +1,20 @@
-
 export default function AboutUs() {
-    return (
-        <section className="py-20 ">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                <div className="lg:text-center">
-                    <h2 className="text-base text-[#00AAFF] font-semibold tracking-wide uppercase">
-                        Tentang Kami
-                    </h2>
-                    <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-                        Siapa Kami? 
-                    </p>
-                    <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
-                        Kami adalah team yang berdedikasi untuk membantu Anda menemukan pekerjaan impian dan membangun karir yang sukses.
-                    </p>
-                </div>
-            </div>
-        </section>
-    );
+  return (
+    <section className="py-20 ">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="lg:text-center">
+          <h2 className="text-base text-[#00AAFF] font-semibold tracking-wide uppercase">
+            Tentang Kami
+          </h2>
+          <p className="mt-2 text-3xl leading-8 font-semibold tracking-tight text-gray-900 sm:text-4xl">
+            Siapa Kami?
+          </p>
+          <p className="mt-4 max-w-2xl lg:text-xl text-gray-500 lg:mx-auto">
+            Kami adalah team yang berdedikasi untuk membantu Anda menemukan
+            pekerjaan impian dan membangun karir yang sukses.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 }
